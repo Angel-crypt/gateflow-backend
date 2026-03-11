4d40a98f26a9aa2086bdf8f347942d89d5ef07e0
Crypt <72517634+Angel-crypt@users.noreply.github.com>
2026-03-11 12:28:44 -0600

    feat(destinations): create initial structure for destinations app with necessary files

---
+++ apps/destinations/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class DestinationsConfig(AppConfig):
+    name = "apps.destinations"
+    label = "destinations"
+    default_auto_field = "django.db.models.BigAutoField"
