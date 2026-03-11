aec3befdabcf6f52d7947bf4fa2228c9a06c2361
Crypt <72517634+Angel-crypt@users.noreply.github.com>
2026-03-11 12:27:26 -0600

    refactor(urls): clean up URL configuration by removing comments and organizing imports

--- config/urls.py
+++ config/urls.py
@@ -1,23 +1,14 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from apps.users.views import ChangePasswordView, CustomTokenObtainPairView, LogoutView, MeView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("auth/login/", CustomTokenObtainPairView.as_view()),
+    path("auth/refresh/", TokenRefreshView.as_view()),
+    path("auth/logout/", LogoutView.as_view()),
+    path("auth/me/", MeView.as_view()),
+    path("auth/change-password/", ChangePasswordView.as_view()),
 ]
