9178bd537dd7b20e5d7c649b9ab221f72a5c99a1
Angel-crypt <angel.cruz.030905@gmail.com>
2026-03-12 17:05:27 -0600

    feat(users): enhance UserListCreateView with filtering and user creation functionality

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,4 +1,4 @@
-from rest_framework import status
+from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -6,7 +6,14 @@ from rest_framework_simplejwt.exceptions import TokenError
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import TokenObtainPairView
 
-from .serializers import ChangePasswordSerializer, CustomTokenObtainPairSerializer, UserSerializer
+from .models import User
+from .permissions import IsAdmin
+from .serializers import (
+    ChangePasswordSerializer,
+    CustomTokenObtainPairSerializer,
+    UserCreateSerializer,
+    UserSerializer,
+)
 
 
 class CustomTokenObtainPairView(TokenObtainPairView):
@@ -42,3 +49,34 @@ class ChangePasswordView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class UserListCreateView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated, IsAdmin]
+
+    def get_serializer_class(self):
+        if self.request.method == "POST":
+            return UserCreateSerializer
+        return UserSerializer
+
+    def get_queryset(self):
+        qs = User.objects.filter(park=self.request.user.park, is_superuser=False).order_by("id")
+
+        role = self.request.query_params.get("role")
+        if role:
+            qs = qs.filter(role=role)
+
+        is_active = self.request.query_params.get("is_active")
+        if is_active is not None:
+            qs = qs.filter(is_active=is_active.lower() == "true")
+
+        return qs
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+    def create(self, request, *args, **kwargs):
+        serializer = UserCreateSerializer(data=request.data, context={"request": request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response(UserSerializer(user).data, status=status.HTTP_201_CREATED)
