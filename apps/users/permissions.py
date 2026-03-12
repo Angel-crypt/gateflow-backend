3edacc340836733c2546861c4bf85055bc84bc70
Angel-crypt <angel.cruz.030905@gmail.com>
2026-03-12 17:04:52 -0600

    feat(permissions): add IsAdmin permission class to restrict access to admin users

---
+++ apps/users/permissions.py
@@ -0,0 +1,8 @@
+from rest_framework.permissions import BasePermission
+from rest_framework.request import Request
+from rest_framework.views import APIView
+
+
+class IsAdmin(BasePermission):
+    def has_permission(self, request: Request, view: APIView) -> bool:
+        return bool(request.user and request.user.is_authenticated and request.user.role == "admin")
