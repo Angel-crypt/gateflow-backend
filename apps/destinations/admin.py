df5fed716163ee66a22d589b360f38774bc0697f
Angel-crypt <angel.cruz.030905@gmail.com>
2026-03-12 19:25:48 -0600

    feat(admin): register Destination and IndustrialPark models with custom admin classes

--- apps/destinations/admin.py
+++ apps/destinations/admin.py
@@ -2,5 +2,18 @@ from django.contrib import admin
 
 from .models import Destination, IndustrialPark
 
-admin.site.register(IndustrialPark)
-admin.site.register(Destination)
+
+@admin.register(Destination)
+class DestinationAdmin(admin.ModelAdmin):
+    model = Destination
+
+    list_display = ["name", "type", "park", "responsible", "is_active"]
+    list_filter = ["type", "is_active", "park"]
+    search_fields = ["name", "park__name", "responsible__email"]
+
+
+@admin.register(IndustrialPark)
+class IndustrialParkAdmin(admin.ModelAdmin):
+    list_display = ["name", "address", "created_at", "is_active"]
+    list_filter = ["is_active"]
+    search_fields = ["name", "address"]
