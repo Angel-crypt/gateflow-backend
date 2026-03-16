180972c0a55b2febc156ac17b45867035553f453
GoldenDiegos <diego.david.lara.martinez@gmail.com>
2026-03-16 15:53:29 -0600

    feat(tests): add initial test structure for IndustrialPark and Destination models

--- apps/destinations/tests.py
+++ apps/destinations/tests.py
@@ -1,1 +1,12 @@
 # Create your tests here.
+from django.test import TestCase
+
+from .models import Destination, IndustrialPark
+
+
+class IndustrialParkModelTest(TestCase):
+    pass
+
+
+class DestinationModelTest(TestCase):
+    pass
