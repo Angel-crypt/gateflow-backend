de6e3290887b1e66fda179ec7bcbd1f7d351cdb6
GoldenDiegos <diego.david.lara.martinez@gmail.com>
2026-03-16 15:42:21 -0600

    feat(views): implement AccessPassListCreateView and AccessPassDetailView for AccessPass model

--- apps/passes/views.py
+++ apps/passes/views.py
@@ -1,1 +1,15 @@
 # Create your views here.
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+
+from .models import AccessPass
+
+
+class AccessPassListCreateView(generics.ListCreateAPIView):
+    queryset = AccessPass.objects.all()
+    permission_classes = [IsAuthenticated]
+
+
+class AccessPassDetailView(generics.RetrieveAPIView):
+    queryset = AccessPass.objects.all()
+    permission_classes = [IsAuthenticated]
