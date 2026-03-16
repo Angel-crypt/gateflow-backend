a182e27ed2fa9ac5b91a9679609f32dcfe4e91c0
GoldenDiegos <diego.david.lara.martinez@gmail.com>
2026-03-16 15:17:19 -0600

    feat(tests): add initial test classes for AccessLog and AccessPass models

--- apps/access/tests.py
+++ apps/access/tests.py
@@ -1,1 +1,12 @@
 # Create your tests here.
+from django.test import TestCase
+
+from .models import AccessLog, AccessPass
+
+
+class AccessLogModelTest(TestCase):
+    pass
+
+
+class AccessPassModelTest(TestCase):
+    pass
