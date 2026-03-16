483312923cc12e52fe47fa70b22e04b294666041
GoldenDiegos <diego.david.lara.martinez@gmail.com>
2026-03-16 15:55:20 -0600

    feat(tests): add tests for creating instances of IndustrialPark and Destination models

--- apps/destinations/tests.py
+++ apps/destinations/tests.py
@@ -8,7 +8,15 @@ class IndustrialParkModelTest(TestCase):
     def test_industrial_park_model_exists(self):
         self.assertIsNotNone(IndustrialPark)
 
+    def test_can_create_industrial_park_instance(self):
+        park = IndustrialPark(name="Test Park")
+        self.assertIsInstance(park, IndustrialPark)
+
 
 class DestinationModelTest(TestCase):
     def test_destination_model_exists(self):
         self.assertIsNotNone(Destination)
+
+    def test_can_create_destination_instance(self):
+        destination = Destination(name="Test Destination")
+        self.assertIsInstance(destination, Destination)
