0e8c4319c93daf9c1f08111a2522d77d314347bf
GoldenDiegos <diego.david.lara.martinez@gmail.com>
2026-03-16 15:52:53 -0600

    feat(views): implement views for IndustrialPark and Destination models with authentication

--- apps/destinations/views.py
+++ apps/destinations/views.py
@@ -1,1 +1,19 @@
-# Create your views here.
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Destination, IndustrialPark
+
+
+class IndustrialParkListView(generics.ListAPIView):
+    queryset = IndustrialPark.objects.all()
+    permission_classes = [IsAuthenticated]
+
+
+class DestinationListCreateView(generics.ListCreateAPIView):
+    queryset = Destination.objects.all()
+    permission_classes = [IsAuthenticated]
+
+
+class DestinationDetailView(generics.RetrieveAPIView):
+    queryset = Destination.objects.all()
+    permission_classes = [IsAuthenticated]
