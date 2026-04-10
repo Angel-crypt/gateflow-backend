90e398daa16681fe838d0a45834e308d75d5fc71
GoldenDiegos <gu26ie0006@globaluniversity.edu.mx>
2026-04-10 11:47:06 -0600

    feat(metrics): register access table URL

--- apps/metrics/urls.py
+++ apps/metrics/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import AccessLogMetricsView, DashboardMetricsView, PassMetricsView
+from .views import AccessLogMetricsView, AccessTableView, DashboardMetricsView, PassMetricsView
 
 urlpatterns = [
     path("dashboard/", DashboardMetricsView.as_view()),
     path("access-logs/", AccessLogMetricsView.as_view()),
     path("passes/", PassMetricsView.as_view()),
+    path("access-table/", AccessTableView.as_view()),
 ]
